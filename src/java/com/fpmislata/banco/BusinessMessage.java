4e8e80dd9ac1bd60c8a3560f90088d5947dc32b1
positronio72 <positronio72@gmail.com>
2014-12-17 12:26:26 +0100

    tienda

---
+++ src/java/com/fpmislata/banco/BusinessMessage.java
@@ -0,0 +1,29 @@
+
+package com.fpmislata.banco;
+
+public class BusinessMessage {
+    
+    String fieldName;
+    String message;
+
+    public BusinessMessage() {
+    }
+    
+    
+    public String getFieldName() {
+        return fieldName;
+    }
+
+    public void setFieldName(String fieldName) {
+        this.fieldName = fieldName;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+    
+}
