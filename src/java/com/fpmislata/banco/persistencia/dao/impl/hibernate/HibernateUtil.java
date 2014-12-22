e0b8482a79e59f41e1f66bde6c7a27035db71ce8
Fran <fmnf72@gmail.com>
2014-12-22 09:37:45 +0100

    banco

--- src/java/com/fpmislata/banco/persistencia/dao/impl/hibernate/HibernateUtil.java
+++ src/java/com/fpmislata/banco/persistencia/dao/impl/hibernate/HibernateUtil.java
@@ -1,6 +1,5 @@
 package com.fpmislata.banco.persistencia.dao.impl.hibernate;
 
-//import javax.imageio.spi.ServiceRegistry;
 
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
@@ -24,42 +23,5 @@ public class HibernateUtil {
     public static synchronized void closeSessionFactory() {
         System.out.println("Hibernate Parado.");
     }
-    
-//    private static SessionFactory sessionFactory;
-//
-//    public static synchronized void buildSessionFactory() {
-//        if (sessionFactory == null) {
-//            Configuration configuration = new Configuration();
-//            configuration.configure();
-//            configuration.setProperty("hibernate.current_session_context_class", "thread");
-//            ServiceRegistry serviceRegistry = new ServiceRegistryBuilder().applySettings(configuration.getProperties()).buildServiceRegistry();
-//            sessionFactory = configuration.buildSessionFactory(serviceRegistry);
-//        }
-//    }
-//
-//    public static void openSessionAndBindToThread() {
-//        Session session = sessionFactory.openSession();
-//        ThreadLocalSessionContext.bind(session);
-//    }
-//
-//    public static SessionFactory getSessionFactory() {
-//        if (sessionFactory == null) {
-//            buildSessionFactory();
-//        }
-//        return sessionFactory;
-//    }
-//
-//    public static void closeSessionAndUnbindFromThread() {
-//        Session session = ThreadLocalSessionContext.unbind(sessionFactory);
-//        if (session != null) {
-//            session.close();
-//        }
-//    }
-//
-//    public static void closeSessionFactory() {
-//        if ((sessionFactory != null) && (sessionFactory.isClosed() == false)) {
-//            sessionFactory.close();
-//        }
-//    }
     
 }
