daf1d678f12d0a423d602767670d930fa695ff36
marti31 <martigomez31@gmail.com>
2015-01-14 09:53:50 +0100

    Merge develop
    Conflicts:
    	web/index3.html

--- web/empleado/app.js
+++ web/empleado/app.js
@@ -8,12 +8,11 @@ app.run(function ($rootScope) {
     $rootScope.estiloBloqueado = {'background-color':'#ffb478', 'font-weight':'bolder'};//Estilo para los input disabled
 });
 
-//app.constant("formatoDatePicker", {
-//    dateFormat: "dd-mm-yy",
-//    dayNamesMin: [ "Do", "Lu", "Ma", "Mi", "Ju", "Vi", "Sa" ],
-//    firstDay: 1,
-//    monthNames: [ "Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre" ],
-//    defaultDate: new Date,
-//    //minDate: new Date(),
-//    showWeek: true
-//});
+app.constant("formatoDatePicker", {   dateFormat: "dd-mm-yy",
+    dayNamesMin: [ "Do", "Lu", "Ma", "Mi", "Ju", "Vi", "Sa" ],
+    firstDay: 1,
+    monthNames: [ "Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre" ],
+    defaultDate: new Date,
+    minDate: new Date(),
+    showWeek: true
+});
