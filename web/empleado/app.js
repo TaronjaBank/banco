aab905dc1e1e33b172d0dbe446add2150e390f5d
Fran <fmnf72@gmail.com>
2015-01-21 01:39:21 +0100

    banco_modelo-detalle

--- web/empleado/app.js
+++ web/empleado/app.js
@@ -23,3 +23,15 @@ app.run(function ($rootScope, $http) {
         };
 });
 
+app.constant('uiDateConfig', {
+    dateFormat: "dd-mm-yy",
+    dayNamesMin: [ "Do", "Lu", "Ma", "Mi", "Ju", "Vi", "Sa" ],
+    firstDay: 1,
+    monthNames: [ "Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre" ],
+    defaultDate: new Date,
+    //minDate: new Date(),
+    maxDate: new Date,
+    showWeek: true,
+    weekHeader: "Sm"
+});
+
