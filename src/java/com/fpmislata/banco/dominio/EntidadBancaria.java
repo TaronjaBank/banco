d3b1ff3e93e6c2f6a48a758c42ad9a4a2fd19cc7
Fran <fmnf72@gmail.com>
2015-02-01 10:58:14 +0100

    banco_cliente

--- src/java/com/fpmislata/banco/dominio/EntidadBancaria.java
+++ src/java/com/fpmislata/banco/dominio/EntidadBancaria.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import java.io.Serializable;
 import java.util.Date;
-import java.util.List;
 import java.util.Set;
 
 @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
