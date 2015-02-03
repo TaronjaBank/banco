2cf0cf6e5db5c4fb013da12a3c8b2f7ca732c51a
Fran <fmnf72@gmail.com>
2015-02-03 17:46:24 +0100

    fran_banco

--- src/java/com/fpmislata/banco/persistencia/migration/V02__insertardatos.sql
+++ src/java/com/fpmislata/banco/persistencia/migration/V02__insertardatos.sql
@@ -7,7 +7,6 @@ ALTER TABLE `movimientoBancario` DISABLE KEYS ;
 ALTER TABLE `sucursalBancaria` DISABLE KEYS ;
 
 
-
 INSERT INTO `entidadbancaria` (`idEntidadBancaria`, `codigoEntidadBancaria`, `nombreEntidadBancaria`, `fechaCreacionEntidadBancaria`) VALUES
 	(1, 'E001', 'Sabadell', '2015-01-18'),
 	(2, 'E002', 'Santander', '2014-10-08'),
@@ -16,8 +15,8 @@ INSERT INTO `entidadbancaria` (`idEntidadBancaria`, `codigoEntidadBancaria`, `no
 	(6, 'E006', 'Bankia2', '2015-01-01');
 
 INSERT INTO `sucursalbancaria` (`idSucursalBancaria`, `codigoSucursalBancaria`, `nombreSucursalBancaria`, `direccionSucursalBancaria`, `idEntidadBancaria`) VALUES
-	(2, 'S002', 'Sucursal2', 'Madrid', 2),
-	(3, 'S003', 'Susursal3', 'Valencia', 1),
+	(2, 'S002', 'Sucursal2', 'Valencia', 2),
+	(3, 'S003', 'Susursal3', 'Madrid', 1),
 	(4, 'S004', 'Sucursal4', 'Barcelona', 2),
 	(5, 'S005', 'prueba', 'prueba', 3),
 	(6, 'S006', 'dsf', 'dfgh', 2),
@@ -25,14 +24,15 @@ INSERT INTO `sucursalbancaria` (`idSucursalBancaria`, `codigoSucursalBancaria`, 
 	(8, 'S008', 'sdf', 'sdf', 4);
 
 INSERT INTO `cliente` (`idCliente`, `dniCliente`, `nombreCliente`, `apellido1Cliente`, `apellido2Cliente`, `loginCliente`, `passwordCliente`) VALUES
-	(1, '123456789-R', 'Marti', 'Gomez', 'Fabia', 'cliente1', 'cliente1'),
-	(2, '987654321-Q', 'Jona', 'Hidalgo', 'Mora', 'cliente2', 'cliente2'),
-	(3, '123456789-A', 'Ivan', 'Sanchez', 'Aaaa', 'cliente3', 'cliente3'),
-	(4, '123456789-B', 'Fran', 'Navarro', 'Flores', 'cliente4', 'cliente4'),
-	(5, 'prueba', 'prueba', 'prueba', 'prueba', 'b', 'b');
+	(1, '00000001R', 'Marti', 'Gomez', 'Fabia', 'cliente1', 'cliente1'),
+	(2, '00000002W', 'Jona', 'Hidalgo', 'Mora', 'cliente2', 'cliente2'),
+	(3, '00000003A', 'Ivan', 'Sanchez', 'Aaaa', 'cliente3', 'cliente3'),
+	(4, '00000004G', 'Fran', 'Navarro', 'Flores', 'cliente4', 'cliente4'),
+	(5, 'prueba', 'prueba', 'prueba', 'prueba', 'b', 'b'),
+        (6, '00000000T', 'Taronja', 'Games', 'SL', 'taronja', 'taronja');
 
 INSERT INTO `cuentabancaria` (`idCuentaBancaria`, `numeroCuentaBancaria`, `idSucursalBancaria`, `idCliente`, `saldoCuentaBancaria`) VALUES
-	(1, 'E002-S002-1', 2, 2, 0.00),
+	(1, 'E002-S002-1', 2, 6, 0.00),
 	(2, 'E002-S002-2', 2, 4, 0.00),
 	(3, 'E001-S003-3', 3, 2, 0.00),
 	(4, 'E002-S004-4', 4, 3, 0.00),
@@ -51,10 +51,10 @@ INSERT INTO `movimientobancario` (`idMovimientoBancario`, `idCuentaBancariaDesti
 
 INSERT INTO `empleado` (`idEmpleado`, `dniEmpleado`, `nombreEmpleado`, `apellido1Empleado`, `apellido2Empleado`, `idSucursalBancaria`, `loginEmpleado`, `passwordEmpleado`) VALUES
 	(5, 'prueba', 'prueba', 'prueba', 'prueba', 5, 'a', 'a'),
-	(1, '12345-A', 'Marti', 'Gomez', 'Fabia', 2, 'login1', 'password1'),
-	(2, '67890-B', 'Jona', 'Hidalgo', 'Mora', 2, 'login2', 'password2'),
-	(3, '12345-B', 'Ivan', 'Sanchez', 'Aaaa', 3, 'login3', 'password3'),
-	(4, '45678-C', 'Fran', 'Navarro', 'Flores', 4, 'login4', 'password4');
+	(1, '00000001R', 'Marti', 'Gomez', 'Fabia', 2, 'login1', 'password1'),
+	(2, '00000002W', 'Jona', 'Hidalgo', 'Mora', 2, 'login2', 'password2'),
+	(3, '00000003A', 'Ivan', 'Sanchez', 'Aaaa', 3, 'login3', 'password3'),
+	(4, '00000004G', 'Fran', 'Navarro', 'Flores', 4, 'login4', 'password4');
 
 
 ALTER TABLE `sucursalBancaria` ENABLE KEYS ;
